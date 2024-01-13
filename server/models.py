33532b98d5a76adaf76bf4fdf8591746f2d7eab5
DidasMba <daniel.mbarushi@gmail.com>
2024-01-13 16:25:10 +0300

    modelsadd

--- server/models.py
+++ server/models.py
@@ -1,9 +1,12 @@
+# server/models.py
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Plant(db.Model, SerializerMixin):
-    __tablename__ = 'plants'
-
+class Plant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    price = db.Column(db.Float)
+
